3bcbec7fd4f0582398ad50fd70a956ad3b179624
Paul Miller <miller_p@hotmail.fr>
2019-11-30 15:10:47 +0100

    Added discriminator accuracy to output and an output txt file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 mb_size = 64
 g_lr = 2e-4
 d_lr = 2e-4
-epochs = 100
+epochs = 250
 CUDA = True
 device = torch.device("cuda" if torch.cuda.is_available() and CUDA else "cpu")
 transform = transforms.ToTensor()
@@ -21,11 +21,17 @@ train_data = torchvision.datasets.MNIST('./data/', download=True,
 train_loader = DataLoader(train_data, shuffle=True, batch_size=mb_size)
 data_iter = iter(train_loader)
 
+test_data = torchvision.datasets.MNIST('./data/', download=True,
+                                       transform=transform, train=False)
+test_loader = DataLoader(test_data, shuffle=False, batch_size=mb_size)
+
 imgs, labels = data_iter.next()
+
+txt_file = 'results/' + str(epochs) + '_epochs/' + 'output.txt'
 
 
 def imshow(imgs, epoch, filename='', save=False,
-           save_dir='results/100_epochs/'):
+           save_dir='results/' + str(epochs) + '_epochs/'):
     imgs = torchvision.utils.make_grid(imgs)
     npimgs = imgs.numpy()
     plt.figure(figsize=(8, 8))
@@ -38,7 +44,6 @@ def imshow(imgs, epoch, filename='', save=False,
 
 
 Z_dim = 100
-H_dim = 12
 X_dim = imgs.view(imgs.size(0), -1).size(1)
 X = int(np.sqrt(X_dim))
 
@@ -134,6 +139,8 @@ for epoch in range(1, epochs+1):
     G_loss_run = 0.0
     D_loss_run = 0.0
     D_fake_sum = 0.0
+    D_right_pred = 0
+    D_fake_right_pred = 0
     for i, (data, _) in enumerate(train_loader):
         data = data.to(device)
         b_size = data.size(0)
@@ -167,12 +174,24 @@ for epoch in range(1, epochs+1):
         D_loss_run += D_loss.item()
         D_fake_sum += torch.sum(D_fake).item()
 
+        with torch.no_grad():
+            D_right_pred += torch.sum(D_real > 0.5)
+            D_fake_right_pred += torch.sum(D_fake < 0.5)
+
+    D_right_pred = 100*D_right_pred/float(len(train_data))
+    D_fake_right_pred = 100*D_fake_right_pred/float(len(train_data))
+
     str_list = ['Epoch: {}', 'G_loss: {:0.5f}', 'D_loss: {:0.5f}',
-                'D_fake_mean: {:0.5f}']
+                'D_fake_mean: {:0.5f}', 'D_real_acc: {:0.3f}%',
+                'D_fake_acc: {:0.3f}%']
     str_out = ', '.join(str_list)
-    D_fake_mean = D_fake_sum/60000
-    print(str_out.format(epoch, G_loss_run/(i+1), D_loss_run/(i+1),
-                         D_fake_mean))
+    D_fake_mean = D_fake_sum/len(train_data)
+    final_out = str_out.format(epoch, G_loss_run/(i+1), D_loss_run/(i+1),
+                               D_fake_mean, D_right_pred, D_fake_right_pred)
+    print(final_out)
+    f = open(txt_file, 'a')
+    f.write(final_out+'\n')
+    f.close()
 
     samples = G(z).detach()
     samples = samples.view(samples.size(0), 1, 28, 28).cpu()
@@ -180,3 +199,58 @@ for epoch in range(1, epochs+1):
         imshow(samples, epoch, filename=str(D_fake_mean)[2:11], save=True)
     else:
         imshow(samples, epoch, filename=str(D_fake_mean)[2:11])
+
+
+with torch.no_grad():
+    G_loss_run = 0.0
+    D_loss_run = 0.0
+    D_fake_sum = 0.0
+    D_right_pred = 0
+    D_fake_right_pred = 0
+    G.eval()
+    D.eval()
+    for i, (data, _) in enumerate(test_loader):
+        data = data.to(device)
+        b_size = data.size(0)
+
+        one_labels = torch.ones(b_size, 1, 1, 1).to(device)
+        zero_labels = torch.zeros(b_size, 1, 1, 1).to(device)
+
+        z = torch.randn(b_size, Z_dim, 1, 1).to(device)
+
+        D_real = D(data)
+        D_fake = D(G(z))
+
+        D_real_loss = F.binary_cross_entropy(D_real, one_labels)
+        D_fake_loss = F.binary_cross_entropy(D_fake, zero_labels)
+
+        D_loss = D_real_loss + D_fake_loss
+
+        z = torch.randn(b_size, Z_dim, 1, 1).to(device)
+        D_fake = D(G(z))
+        G_loss = F.binary_cross_entropy(D_fake, one_labels)
+
+        G_loss_run += G_loss.item()
+        D_loss_run += D_loss.item()
+        D_fake_sum += torch.sum(D_fake).item()
+
+        D_right_pred += torch.sum(D_real > 0.5)
+        D_fake_right_pred += torch.sum(D_fake < 0.5)
+
+    D_right_pred = 100*D_right_pred/float(len(test_data))
+    D_fake_right_pred = 100*D_fake_right_pred/float(len(test_data))
+
+    str_list = ['Test epoch: {}', 'G_loss: {:0.5f}', 'D_loss: {:0.5f}',
+                'D_fake_mean: {:0.5f}', 'D_real_acc: {:0.3f}%',
+                'D_fake_acc: {:0.3f}%']
+    str_out = ', '.join(str_list)
+    D_fake_mean = D_fake_sum/len(test_data)
+    final_out = str_out.format(epoch, G_loss_run/(i+1), D_loss_run/(i+1),
+                               D_fake_mean, D_right_pred, D_fake_right_pred)
+    print(final_out)
+    f = open(txt_file, 'a')
+    f.write(final_out+'\n')
+    f.close()
+
+    samples = G(z).detach()
+    samples = samples.view(samples.size(0), 1, 28, 28).cpu()
